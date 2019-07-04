867b477b91560f3f0fde7472c7f7bafb58b3753f
Cate Miller <cate.marie.m@gmail.com>
2019-07-04 16:34:12 -0700

    replace defaults with basic static content

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MusicSnob msg="find only the music you like"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MusicSnob from './components/MusicSnob.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MusicSnob
   }
 }
 </script>
---
+++ src/components/MusicSnob.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      city: [options go here]
+    </p>
+    <p>
+      genre: [options go here]
+    </p>
+    <p>
+      <h3>calendar</h3>
+      <ul>
+        <li>[results]</li>
+        <li>[go]</li>
+        <li>[here]</li>
+      </ul>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicSnob',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
