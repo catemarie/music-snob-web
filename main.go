730790bb03a7aafddaec73fceca3f990b6fe8eb0
Cate Miller <cate.marie.m@gmail.com>
2019-07-04 16:03:36 -0700

    right now let's hardcode this to trance

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ func main() {
     // get results for a city/state - San Diego, CA
     state := "California"
     city := "San%20Diego"
+    genre := "trance"
 
     loc_id := get_location_id(state, city)
     artists := get_artists(loc_id)
     for _, artist  := range artists {
-    	log.Println(artist)
     	genres :=  get_artist_genres(artist)   
-	    for _, genre := range genres {
-	        log.Println("    * " + genre)
+	    for _, g := range genres {
+            if (g == genre) {
+                log.Println(artist)
+                break
+            }
 	    }
     }
 }
