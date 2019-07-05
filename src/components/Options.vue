1b08477457f2f44bf7d3ac1f875c2c68035820ec
Cate Miller <cate.marie.m@gmail.com>
2019-07-04 23:56:13 -0700

    put both dropdowns in the same row

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="options">
-    <div class="dropdown">
-      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-        city
-        <span class="caret"></span>
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-        <li role="separator" class="divider"></li>
-        <li><a href="#">{{ msg }}</a></li>
-      </ul>
-    </div>
 
-    <div class="dropdown">
-      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-        genre
-        <span class="caret"></span>
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-        <li role="separator" class="divider"></li>
-        <li><a href="#">{{ msg }}</a></li>
-      </ul>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              city
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+              <li role="separator" class="divider"></li>
+              <li><a href="#">{{ msg }}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              genre
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+              <li role="separator" class="divider"></li>
+              <li><a href="#">{{ msg }}</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
