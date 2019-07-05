32097d26f93ff1bdc6f6c0d3deebd59ab1600335
Cate Miller <cate.marie.m@gmail.com>
2019-07-05 15:20:00 -0700

    writing results to JSON

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+    "fmt"
 	"log"
+    "net/http"
+    "encoding/json"
 )
+
+type Results struct {
+    Events   []string
+}
+
+var entries []string 
+
+func render_entries(w http.ResponseWriter, r *http.Request){
+    calendar_content := Results{
+        Events: entries,
+    }
+
+    var jsonData []byte
+    jsonData, err := json.Marshal(calendar_content)
+    if err != nil {
+        log.Println(err)
+    }
+    fmt.Fprintf(w, string(jsonData))
+}
 
 func main() {
 
@@ -17,9 +39,14 @@ func main() {
     	genres :=  get_artist_genres(artist)   
 	    for _, g := range genres {
             if (g == genre) {
-                log.Println(artist)
+                entries = append(entries, artist)
                 break
             }
 	    }
     }
+
+    log.Println("entries: ", entries)
+
+    http.HandleFunc("/artists/", render_entries)
+    http.ListenAndServe(":8080", nil)
 }
