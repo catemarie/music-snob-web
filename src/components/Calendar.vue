65e65943f328d594caaed812f85833bf722fc713
Cate Miller <cate.marie.m@gmail.com>
2019-07-04 23:52:46 -0700

    add initial bootstrap components, although ugly

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,10 +2,10 @@
   <div class="calendar">
     <p>
       <h3>calendar</h3>
-      <ul>
-        <li>{{ msg }}</li>
-        <li>{{ msg }}</li>
-        <li>{{ msg }}</li>
+      <ul class="list-group">
+        <li class="list-group-item">{{ msg }}</li>
+        <li class="list-group-item">{{ msg }}</li>
+        <li class="list-group-item">{{ msg }}</li>
       </ul>
     </p>
   </div>
--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="options">
-    <p>
-      city: {{ msg }}
-    </p>
-    <p>
-      genre: {{ msg }}
-    </p>
+    <div class="dropdown">
+      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        city
+        <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <li role="separator" class="divider"></li>
+        <li><a href="#">{{ msg }}</a></li>
+      </ul>
+    </div>
+
+    <div class="dropdown">
+      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        genre
+        <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <li role="separator" class="divider"></li>
+        <li><a href="#">{{ msg }}</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
